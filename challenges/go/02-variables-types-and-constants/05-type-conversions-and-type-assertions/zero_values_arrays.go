4a78f4a7330a5ed913f8e1abf26d76cdccae5bc8
oscar campos <oscar.campos@delfosti.com>
2026-03-29 12:36:00 -0500

    vault backup: 2026-03-29 12:36:00

---
+++ challenges/go/02-variables-types-and-constants/05-type-conversions-and-type-assertions/zero_values_arrays.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nums [5]int
+	var flags [3]bool
+	var names [2]string
+
+	fmt.Printf("nums: %v\n",nums)
+	fmt.Printf("flags: %v\n",flags)
+	fmt.Printf("names: %q\n",names)
+}
