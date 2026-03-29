e491a6dfe2cc578b32bca4adad99d692259b3c66
oscar campos <oscar.campos@delfosti.com>
2026-03-29 15:36:58 -0500

    vault backup: 2026-03-29 15:36:58

--- challenges/go-concepts/concurrency/01-goroutines-and-scheduling/01-launching-goroutines/main.go
+++ challenges/go-concepts/concurrency/01-goroutines-and-scheduling/01-launching-goroutines/main.go
@@ -8,10 +8,30 @@ import (
 
 func printNumbers(label string){
 
+		for i:=0; i<5; i++{
+			fmt.Print("%s-%d",label,i)
+			time.Sleep(20*time.Millisecond)
+		}
+		fmt.Println()
 }
 
 
 func main() {
-	
+	fmt.Println("--- Sequential ---")
+	start := time.Now()
+
+	printNumbers("A")
+	printNumbers("B")
+	printNumbers("C")
+
+	fmt.Printf("Sequential took: %v\n\n",time.Since(start).Round(time.Millisecond))
+
+
+	fmt.Println("--- Concurrent ---")
+	start:= time.Now()
+
+	var wg sync.WaitGroup
+
+
 }
 
