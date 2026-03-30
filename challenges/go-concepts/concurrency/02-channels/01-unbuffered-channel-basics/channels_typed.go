a6baeeb3e58e8eb027626a02af9835d15f1c8ba1
oscar campos <oscar.campos@delfosti.com>
2026-03-29 20:44:37 -0500

    vault backup: 2026-03-29 20:44:37

---
+++ challenges/go-concepts/concurrency/02-channels/01-unbuffered-channel-basics/channels_typed.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Point struct{X,Y int}
+
+
+func main(){
+	pointCh := make(chan Point)
+	go func(){
+		pointCh <- Point{3,4}
+	}
+	
+	p:=<-pointCh
+	fmt.Println("Point received:",p)
+	errCh := make(chan error)
+
+	go func(){
+		errCh <- fmt.Errorf("something went wrong")
+	}()
+
+	err := <- errCh
+	fmt.Println("error received:",err)
+}
+
+
+
+
+
+
