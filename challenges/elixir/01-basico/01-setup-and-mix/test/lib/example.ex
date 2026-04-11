00ea3aa30ff802d2630d20ba48207b875ea98573
oscar campos <oscar.campos@delfosti.com>
2026-04-11 06:50:26 -0500

    vault backup: 2026-04-11 06:50:26

--- challenges/elixir/01-basico/01-setup-and-mix/test/lib/example.ex
+++ challenges/elixir/01-basico/01-setup-and-mix/test/lib/example.ex
@@ -5,8 +5,15 @@ defmodule Test do
 
   @doc "pattern matching con tuplas"
   def tupla_example do
-    {x,y}={10,20}
-    x+y
+    {x, y} = {10, 20}
+    x + y
   end
 
+  def dividir(a, b) do
+    if b == 0 do
+      {:error, "division por cero"}
+    else
+      {:ok, a / b}
+    end
+  end
 end
--- challenges/elixir/01-basico/01-setup-and-mix/test/scripts/main.exs
+++ challenges/elixir/01-basico/01-setup-and-mix/test/scripts/main.exs
@@ -1,2 +1,6 @@
 
-IO.puts("tupla_ejemplo #{Test.tupla_example()}")
+
+case Test.dividir(10,2) do
+  {:ok,resultado} -> IO.puts("resultado: #{resultado}")
+  {:error,error} -> IO.puts("error: #{error}")
+end
