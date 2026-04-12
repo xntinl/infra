b4cb24db0371d85a11ab1964a8b541d91107c78c
oscar campos <oscar.campos@delfosti.com>
2026-04-12 06:58:15 -0500

    vault backup: 2026-04-12 06:58:15

--- challenges/elixir/01-basico/01-setup-and-mix/json_validator/lib/json_validator.ex
+++ challenges/elixir/01-basico/01-setup-and-mix/json_validator/lib/json_validator.ex
@@ -1,18 +1,3 @@
 defmodule JsonValidator do
-  @moduledoc """
-  Documentation for `JsonValidator`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> JsonValidator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
